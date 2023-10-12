44c7405a4fb1d7793d930e5b1f6aee825f74939c
Bartlomiej Adamczewski <bartlomiej.adamczewski@gmail.com>
2023-10-12 20:42:29 +0200

    Added necessary links and scripts to header.php and footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -6,8 +6,18 @@
   src="https://code.jquery.com/jquery-3.7.1.min.js"
   integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
   crossorigin="anonymous"></script>
+
+  <!-- jQuery validate plugin -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
+
+  <!-- jQuery Date and Time Picker -->
   <script src="/js/jquery.datetimepicker.full.min.js"></script>
+
+  <!-- Bootstrap library script -->
+  <script 
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
+  integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" 
+  crossorigin="anonymous"></script>
   
   <script src="/js/script.js"></script>
 </body>
