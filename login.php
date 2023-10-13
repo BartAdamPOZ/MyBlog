0471d22213d80f6cf8c0a46b87b2ebaa152ccf7b
Bartlomiej Adamczewski <bartlomiej.adamczewski@gmail.com>
2023-10-14 01:01:36 +0200

    Added basic styling for login form.

--- login.php
+++ login.php
@@ -18,24 +18,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 }
 ?>
 <?php require 'includes/header.php'; ?>
-<h2>Login</h2>
 
-<?php if ( ! empty($error)) : ?>
+<div class="form-signin w-100 m-auto">
+  <h1 class="h3 mb-3 fw-normal">Login</h1>
+
+  <?php if ( ! empty($error)) : ?>
   <p><?= $error ?></p>
-<?php endif;?>
+  <?php endif;?>
 
-<form method='post'>
-  <div class="form-group">
-    <label for="username">Username: </label>
-    <input type="text" id="username" name="username" class="form-control">
-  </div>
+  <form method='post'>
 
-  <div class="form-group">
-    <label for="password">Password: </label>
-    <input type="password" id="password" name="password" class="form-control">
-  </div>
+    <div class="form-floating">
+      
+      <input type="text" id="username" name="username" class="form-control">
+      <label for="username">Username: </label>
 
-  <button>Log in</button>
-</form>
+    </div>
+    <div class="form-floating">
+      
+      <input type="password" id="password" name="password" class="form-control">
+      <label for="password">Password: </label>
+    </div>
 
+    <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+  </form>
+</div>
 <?php require 'includes/footer.php'; ?>
