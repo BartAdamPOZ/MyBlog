3cf9c0908bba2744ace82ce5f5fcd8748d0715ca
Bartlomiej Adamczewski <bartlomiej.adamczewski@gmail.com>
2023-10-13 23:37:00 +0200

    Added header template with responsive hamburger menu.

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
   <!-- Bootstrap library -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
   <!-- jQuery Date and Time Picker library -->
   <link rel="stylesheet" href="/css/jquery.datetimepicker.min.css">
@@ -15,33 +18,38 @@
 
   <title>My blog</title>
 </head>
+
 <body>
 
   <div class="container">
- 
-  <header>
-    <h1>My Blog</h1>
-  </header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary my-3">
+      <div class="container d-flex py-2">
+        <h1 class="mx-3">MyBlog</h1>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="/">Home</a>
+            </li>
 
-  <nav>
-    <ul class="nav">
+            <?php if (Auth::isLoggedIn()) :?>
 
-      <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="/admin/">Admin</a></li>
+            <li class="nav-item"><a class="nav-link" href="/logout.php">Log out</a></li>
 
-      <?php if (Auth::isLoggedIn()) :?>
+            <?php else :?>
 
-        <li class="nav-item"><a class="nav-link" href="/admin/">Admin</a></li>
-        <li class="nav-item"><a class="nav-link" href="/logout.php">Log out</a></li>
+            <li class="nav-item"><a class="nav-link" href="/login.php">Log in</a></li>
 
-      <?php else :?>
+            <?php endif; ?>
 
-        <li class="nav-item"><a class="nav-link" href="/login.php">Log in</a></li>
+            <li class="nav-item"><a class="nav-link" href="/contact.php">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
-      <?php endif; ?>
-
-      <li class="nav-item"><a class="nav-link" href="/contact.php">Contact</a></li>
-
-    </ul>
-  </nav>
-
-  <main>
+    <main>
